af0b496eec7384263807a9b67609cbd59606e150
sharifzadran <sharif.z.zadran@gmail.com>
2022-09-21 17:00:05 -0700

    first commit

---
+++ javaVariables.java
@@ -0,0 +1,15 @@
+package com.TCP.SummerPracticeSelf;
+
+public class javaVariables {
+
+	public static void main(String[] args) {
+	
+	int a, b = 7, c = 8, d;
+	
+	d = 10;
+	a = b + c + d;
+	    }
+	}
+	
+
+
